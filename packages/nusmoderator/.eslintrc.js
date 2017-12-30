ba67b29b0a5b401a0ff33465588fe4fba5f1d889
Li Kai <falconets25601@gmail.com>
2017-12-30 23:48:13 +0800

    Hoist linting to root

--- packages/nusmoderator/.eslintrc.js
+++ packages/nusmoderator/.eslintrc.js
@@ -1,31 +1,9 @@
 module.exports = {
-  parser: 'babel-eslint',
-  root: true,
-  extends: ['airbnb-base', 'plugin:flowtype/recommended'],
   env: {
-    browser: true,
     node: true,
+    browser: true,
   },
-  plugins: ['import', 'flowtype'],
-  overrides: [
-    {
-      files: '**/*.test.{js,jsx}',
-      env: {
-        jest: true,
-      },
-    },
-  ],
-  rules: {
-    'arrow-body-style': 'off',
-    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
-    'max-len': ['error', 120],
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        js: 'never',
-      },
-    ],
-    'linebreak-style': 'off',
+  settings: {
+    'import/resolver': 'node',
   },
 };
